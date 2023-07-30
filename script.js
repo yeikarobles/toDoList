1c0a2d1bf030cbb0ae02e0c958e68b853ace6b14
Yeika Robles <yeikarobles@gmail.com>
2023-07-30 00:43:53 -0400

    added icons for future functions: edit, delete

--- script.js
+++ script.js
@@ -2,34 +2,66 @@
 
 const lista = [];
 const elementoss = document.getElementById("elementos");
+const wrapper = document.getElementById("wrapper");
 const cosasAgregadas = document.getElementById("cosasAgregadas");
 const addBtn = document.getElementById("addBtn");
 
-//crear una funcion para que cuando punches el btn se agregue el elemento a la lista
-
+//boton para agregar thing to do
 addBtn.addEventListener("click", () => {
-  //si texto value = "" return;
-  if (cosasAgregadas.value == "") {
+  //necesito hacer un validador de texto!
+  if (cosasAgregadas.value === "") {
     return;
   }
+
+  enlistar();
+  nuevoElemento();
+  cosasAgregadas.value = "";
+});
+
+//agregar elemento a lista
+function enlistar() {
   lista.push(cosasAgregadas.value);
-  console.log(lista);
+}
 
-  // por cada objeto de la lista?
-
-  //por cada elemento en lista => haz esto = crear nuevo elemento
+//crear nuevo elemento:
+function nuevoElemento() {
+  //creando checkbox + to do
   let contenedor = document.createElement("div");
   let listo = document.createElement("input");
   listo.setAttribute("type", "checkbox");
   let elemento = document.createElement("p");
+
   elemento.innerHTML = cosasAgregadas.value;
 
+  //creando botones
+  let btnEdit = document.createElement("button");
+  let btnDelete = document.createElement("button");
+  //e iconos
+  let iEdit = document.createElement("i");
+  let iDelete = document.createElement("i");
+  iEdit.className = "fa-solid fa-pen-to-square";
+  iDelete.className = "fa-solid fa-xmark";
+  //iconos dentro de botones:
+  btnEdit.appendChild(iEdit);
+  btnDelete.appendChild(iDelete);
+
+  // div para los botones
+  let wrapper = document.createElement("div");
+
+  wrapper.appendChild(btnEdit);
+  wrapper.appendChild(btnDelete);
+
+  //donde deben estar los elementos en el html
   contenedor.appendChild(listo);
   contenedor.appendChild(elemento);
-  console.log(elementoss);
   elementoss.appendChild(contenedor);
+  elementoss.appendChild(wrapper);
+}
 
-  cosasAgregadas.value = "";
-});
+//eliminar objetos de la lista
+//editar algun objeto de la lista
 
-//funcion para eliminar objetos de la lista??
+function editar() {}
+function eliminar() {}
+
+//funcion para eliminar todos los objetos de la lista
